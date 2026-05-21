5f8405e514dc275f5da75c333c92261833e46d43
persiswilson <persis2508@gmail.com>
2026-05-21 12:57:48 +0530

    Update API_URL to point to live Render production backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://fastapi-task-manager-rxqc.onrender.com";
 
 document.addEventListener("DOMContentLoaded", () => {
     const token = localStorage.getItem("token");
